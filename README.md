90c4011bead4be107c851c5c74f459efbba5c4dc
Grant Rolls <grant@allumeenergy.com.au>
2025-09-16 13:38:54 +1000

    Refactor scripts

--- README.md
+++ README.md
@@ -1,80 +1,70 @@
 # Syncronised dotfiles
 
 [A guide on dotfiles](https://dotfiles.github.io/)
+
 [Inspired-by and built-upon](https://github.com/mathiasbynens/dotfiles)
+
+## Overview
 
 ```plantuml
 @startuml
 [*] --> CheckArguments : Start
-CheckArguments --> PullLatest : --pull flag provided
-CheckArguments --> InstallDependencies : No pull flag
-PullLatest --> InstallDependencies
-InstallDependencies --> SyncDotfiles
-SyncDotfiles --> SourceBashProfile : Source .bash_profile
-SourceBashProfile --> InstallPackages
-InstallPackages --> Cleanup
-Cleanup --> [*] : Finish
+CheckArguments --> InstallDependencies
+
+InstallDependencies --> CheckoutBranch : if `--branch` flag provided, checkout branch\n else checkout `main`
+CheckoutBranch --> PullLatest : --pull flag provided\n(use for updates)
+CheckoutBranch --> SyncDotfiles : No pull flag
+PullLatest --> SyncDotfiles
+SyncDotfiles --> InstallPackages : Source .bash_profile
+InstallPackages --> Cleanup : Source .bash_profile
+Cleanup --> [*] : Finish 
 @enduml
 ```
 
-## Pre-requisites
+## Installation
 
+**Warning:** If you want to give these dotfiles a try, you should first fork this repository, review the code, and remove things you don’t want or need.
+
+### Pre-requisites
+
+* curl
 * git
 * rsync
 * bash
 
-## Installation
-
-**Warning:** If you want to give these dotfiles a try, you should first fork this repository, review the code, and remove things you don’t want or need. Don’t blindly use my settings unless you know what that entails. Use at your own risk!
-
 ### Using Git and the bootstrap script
 
-You can clone the repository wherever you want. (I like to keep it in `~/workspace/dot`) The bootstrapper script will pull in the latest version and copy the files to your home folder.
+You can clone the repository wherever you want. (I like to keep it in `~/dot`) The bootstrapper script will pull in the latest version and copy the files to your home folder.
 
 ```bash
-git clone <this/repo-url> ~/workspace/dot && cd ~/workspace/dot && source bootstrap.sh
-```
-
-To update, `cd` into your local `dotfiles` repository and then:
-
-```bash
-source bootstrap.sh
-```
-
-Alternatively, to update while avoiding the confirmation prompt:
-
-```bash
-set -- -f; source bootstrap.sh
-```
-
-### Specify the `$PATH`
-
-If `~/.path` exists, it will be sourced along with the other files, before any feature testing takes place.
-
-Here’s an example `~/.path` file that adds `/usr/local/bin` to the `$PATH`:
-
-```bash
-export PATH="/usr/local/bin:$PATH"
+git clone <this/repo-url> ~/dot && cd ~/dot && source bootstrap.sh
 ```
 
 ### Add custom commands without creating a new fork
 
 If `~/.extra` exists, it will be sourced along with the other files. You can use this to add a few custom commands without the need to fork this entire repository, or to add commands you don’t want to commit to a public repository.
 
-My `~/.extra` looks something like this:
+eg.
 
 ```bash
 # Git credentials
 GIT_AUTHOR_NAME="Grant Rolls"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="grant.rolls@design-industry.com.au"
+GIT_AUTHOR_EMAIL="my-email@email.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
+git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 ```
 
 ## Syncronising/Updating
 
 ```bash
-cd ~/workspace/dot && source bootstrap.sh
+cd ~/dot && source bootstrap.sh --pull
+```
+
+### To specify a branch (for scripted/devcontainer setups)
+
+```bash
+cd ~/dot && source bootstrap.sh --pull --branch <branch>
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,44 @@ USER=$(id -un)
 USER_GROUP=$(id -gn)
 
 pull=false
+branch="main"
 
-for arg in "$@"; do
-	case $arg in
+while [[ $# -gt 0 ]]; do
+    case $1 in
 	--pull | -p)
 		pull=true
+		shift
+		;;
+
+	--branch | -b)
+		branch=$2
+		shift 2
 		;;
 	esac
 done
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-if $pull; then
-	echo "Pulling latest"
-	git pull origin main
-fi
-
 function installDependencies() {
 	echo "Installing Dependencies"
 	sudo apt-get update
-	if ! sudo apt-get install -y git rsync; then
+	if ! sudo apt-get install -y git rsync curl; then
 		echo "Failed to install dependencies"
 		exit 1
+	fi
+}
+
+function dotsUpdate {
+	branch=$1
+	pull="${2:-false}"
+
+	if git rev-parse --verify "$branch" >/dev/null 2>&1; then
+		git checkout "$branch"
+	fi
+
+	if [ "$pull" = true ]; then
+		echo "Pulling latest of $branch"
+		git pull origin $branch
 	fi
 }
 
@@ -36,97 +52,31 @@ function syncDotfiles() {
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
+		--exclude "dotsInstallPackages" \
+		--exclude "dotsUpdate" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . $HOME
 	echo "dotfiles have been synced!"
 }
 
-function installPackages() {
-	echo "🚀 Installing Packages"
-
-	if [ ! -d "$HOME/.local/bin" ]; then
-		mkdir -p $HOME/.local/bin
-		sudo chown $USER:$USER_GROUP $HOME/.local/bin
-		echo "💾 Created $HOME/.local/bin"
-	fi
-
-	# Install vim, if not already installed
-	if ! command -v vim &>/dev/null; then
-		echo "💻 Installing vim"
-		sudo apt-get install -y vim
-	else
-		echo "vim is already installed"
-	fi
-
-	# Install laygit
-	# https://github.com/jesseduffield/lazygit
-	if ! command -v lazygit &>/dev/null; then
-		echo "⌨️ Installing lazygit"
-
-		LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-		curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-		tar xf lazygit.tar.gz lazygit
-		install lazygit -D -t $HOME/.local/bin/
-	else
-		echo "lazygit is already installed"
-	fi
-
-	# Install lazydocker
-
-	if ! command -v lazydocker &>/dev/null; then
-		echo "🐋 Installing lazydocker"
-
-		curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-	else
-		echo "lazydocker is already installed"
-	fi
-
-	# Install starship prompt
-	# https://starship.rs/
-	if ! command -v starship &>/dev/null; then
-		echo "🚀 Installing starship"
-		curl -sS https://starship.rs/install.sh -o ss-install.sh
-		sh ss-install.sh --bin-dir $HOME/.local/bin -y
-		rm ss-install.sh
-	else
-		echo "starship is already installed"
-	fi
-
-	# Install uv
-	# https://astral.sh/uv/
-	if ! command -v uv &>/dev/null; then
-		echo "🐍 Installing py manager (uv)…"
-
-		curl -LsSf https://astral.sh/uv/install.sh | sh
-	else
-		echo "py manager (uv) is already installed"
-	fi
-	source $HOME/.local/bin/env
-
-	uv python install $PY_VERSION
-	echo "🔧 uv using py version $PY_VERSION"
-
-	uv tool install copier
-	echo "🔧 Installed copier (via uv)"
-
-	echo "✅ Packages have been installed!"
-}
-
 function cleanup {
 	sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
+	unset installDependencies dotsUpdate syncDotfiles
 }
 
 installDependencies
+dotsUpdate
 
 syncDotfiles
 source $HOME/.bash_profile
 
-installPackages
+source dotsInstallPackages
+dotsInstallPackages
+unset dotsInstallPackages
+
 source $HOME/.bash_profile
 
 cleanup
-
-unset installDependencies syncDotfiles installPackages cleanup
 
 echo "💪 Bootstrap Complete"
