0e2e0224a8552d56f0055d5bcefc21e1000aac70
Grant Rolls <grant@allumeenergy.com.au>
2025-09-29 13:27:14 +1000

    test no gitcconfig at all

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,19 +66,19 @@ PERSONAL_GITCONFIG=".my-gitconfig"
 TARGET_GITCONFIG="$HOME/.gitconfig"
 INCLUDE_LINE="[include]\n\tpath = ~/${PERSONAL_GITCONFIG}"
 
-# Check if .gitconfig exists and if it already includes the personal config
-if [ -f "$TARGET_GITCONFIG" ]; then
-    if ! grep -q "path = ~/my-personal.gitconfig" "$TARGET_GITCONFIG"; then
-        echo -e "\n$INCLUDE_LINE" >> "$TARGET_GITCONFIG"
-        echo "Added include to .gitconfig"
-    else
-        echo "Include already present in .gitconfig"
-    fi
-else
-    # Create .gitconfig with the include if it doesn't exist
-    echo -e "$INCLUDE_LINE" > "$TARGET_GITCONFIG"
-    echo ".gitconfig created with include"
-fi
+# # Check if .gitconfig exists and if it already includes the personal config
+# if [ -f "$TARGET_GITCONFIG" ]; then
+#     if ! grep -q "path = ~/my-personal.gitconfig" "$TARGET_GITCONFIG"; then
+#         echo -e "\n$INCLUDE_LINE" >> "$TARGET_GITCONFIG"
+#         echo "Added include to .gitconfig"
+#     else
+#         echo "Include already present in .gitconfig"
+#     fi
+# else
+#     # Create .gitconfig with the include if it doesn't exist
+#     echo -e "$INCLUDE_LINE" > "$TARGET_GITCONFIG"
+#     echo ".gitconfig created with include"
+# fi
 
 }
 
